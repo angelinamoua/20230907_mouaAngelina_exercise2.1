5425fda783025286fa86a80b94c002c1794cfc32
mouaa <mouaa@DESKTOP-NT2AJPK>
2023-09-07 15:57:02 -0400

    Add project files.

---
+++ 20230907_mouaAngelina_exercise2.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InchesToCentimeters
+{
+    internal class Program
+    { 
+        static void Main(string[] args)
+        {
+            const double cent = 2.54;
+            var inch = 15994;
+            Console.WriteLine(inch + " inches is " + cent * inch + " centimeters");
+            Console.ReadLine();
+        }
+    }
+}
